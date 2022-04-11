c3a5a167a2d8192e50559093a4b0d8a324c79175
Infinite Canvas <admin@infinitecanvas.io>
2022-04-11 07:31:56 +0000

    reworked code for practice and fixed group cropping

--- main.py
+++ main.py
@@ -1,153 +1,16 @@
-import itertools
 import pathlib
-import os
-import re
+import shutil
 import sys
 import argparse
-from os import path as p
-from PIL import Image
-from alive_progress import alive_bar
+from processing import *
 
 
-class Object(object):
-    pass
-
-
-def autocrop_image(image: Image, border: (int, int) = (0, 0)):
-    """
-    :param image: Image to crop
-    :param border: Border to add
-    :return: Cropped Image
-    """
-    # Get the bounding box
-    bbox = image.getbbox()
-    # Crop the image to the contents of the bounding box
-    cropped = image.crop(bbox)
-    # Determine the width and height of the cropped image
-    (width, height) = cropped.size
-    # Add border
-    width += border[0] * 2
-    height += border[1] * 2
-    # Create a new image object for the output image
-    cropped_image = Image.new("RGBA", (width, height), (0, 0, 0, 0))
-    # Paste the cropped image onto the new image
-    cropped_image.paste(cropped, (border[0], border[1]))
-
-    return cropped_image, (bbox[0], bbox[1])
-
-
-def get_area(coords: (int, int, int, int)):
-    width = coords[2] - coords[0]
-    height = coords[3] - coords[1]
-    result = Object()
-    result.bbox = coords
-    result.area = width * height
-    return result
-
-
-def group_crop_images(images: [Image], border: (int, int) = (0, 0), callback=None):
-    """
-    :param images:
-    :param border:
-    :return:
-    """
-    bboxes = [get_area(image.getbbox()) for image in images]
-    bbox = sorted(bboxes, key=lambda x: x.area, reverse=True)[0].bbox  # monkey patched
-    cropped_images = []
-    for image in images:
-        # Crop the image to the contents of the bounding box
-        cropped = image.crop(bbox)
-        # Determine the width and height of the cropped image
-        (width, height) = cropped.size
-        # Add border
-        width += border[0] * 2
-        height += border[1] * 2
-        # Create a new image object for the output image
-        cropped_image = Image.new("RGBA", (width, height), (0, 0, 0, 0))
-        # Paste the cropped image onto the new image
-        cropped_image.paste(cropped, (border[0], border[1]))
-        cropped_images.append((cropped_image, (bbox[0], bbox[1])))
-        if callback:
-            callback()
-    return cropped_images
-
-
-def process_image_files(paths: [str], **kwargs) -> [(Image, [int, int])]:
-    """
-    :param paths: Paths to the image files
-    :return: Returns the cropped image and coordinates of top-left bounding box
-    """
-    results = []
-    # load image files
-    originals = [Image.open(path) for path in paths]
-    # crop images
-    cropped_images = group_crop_images(originals, (kwargs['border_width'], kwargs['border_height']), kwargs['callback'])
-
-    for cropped, (x, y) in cropped_images:
-        result = Object()
-        result.image = cropped
-        result.x = x
-        result.y = y
-        results.append(result)
-    return results
-
-
-def process_image_file(path: str, **kwargs) -> (Image, [int, int]):
-    """
-    :param path: Path to the image file
-    :return: Returns the cropped image and coordinates of top-left bounding box
-    """
-    # Load image file
-    original = Image.open(path)
-    # Crop image
-    result = Object()
-    cropped, pos = autocrop_image(original, (kwargs['border_width'], kwargs['border_height']))
-    result.image = cropped
-    result.x = pos[0]
-    result.y = pos[1]
-    return result
-
-
-def save_image_to_file(image: Image, path: str):
-    """
-    :param image: Image  to save
-    :param path: Path to save image to
-    :return: None
-    """
-    image.save(path)
-
-
-def save_coordinates_to_file(name: str, x: int, y: int, path: str, formatting):
-    with open(path, mode='a+') as txt:
-        txt.write(formatting.format(name=name, x=x, y=y))
-
-
-def get_folders_and_images(path: str, extensions=["jpg", "png"]):
-    images_dict = {}
-    grouped_dict = {}
-    print(path)
-    # walk through all sub folders
-    for currentdir, subdirs, files in os.walk(path):
-        # get list of files with defined extensions
-        images = list(filter(lambda filename: any(f".{e}" in filename for e in extensions), files))
-        # process unless there are no image files or is in cropped folder
-        if len(images) > 0 and 'cropped' not in currentdir:
-            rel = p.relpath(currentdir, path)
-            # group images that match (.*_(idle|hover)\.(extensions))
-            r = re.compile("(.+)(_morning|_afternoon|_noon|_evening|_night)*(_hover|_idle)")
-            grouped_images = itertools.groupby(images, lambda x: r.match(x).groups()[0] if r.match(x) else None)
-            grouped_images = list(map(lambda x: (x[0], list(x[1])), grouped_images))
-            grouped_dict[rel] = grouped_images, len(images)
-            images_dict[rel] = images
-    return images_dict, grouped_dict
-
-
-def get_work_dir(path):
+def get_dir(path):
     path = pathlib.Path(path)
-    if not pathlib.Path.is_dir(path):
-        return str(path.parent)
+    if path.is_dir():
+        return str(path.absolute())
     else:
-        return str(path)
+        return str(path.parent.absolute())
 
 
 def parse_arguments(args) -> dict:
@@ -157,103 +20,78 @@ def parse_arguments(args) -> dict:
     """
     parser = argparse.ArgumentParser(prog="Auto Cropper Tool",
                                      description="Processes all files with specified extensions in given image folders.")
-    parser.add_argument('-w', '--work-dir', default=sys.path[0], nargs='?', type=get_work_dir,
-                        help="Set the working directory. Cropped images are put here. Default is script starting directory.")
+    parser.add_argument('-i', '--input', default=sys.path[0], nargs='?', type=get_dir,
+                        help="Set the working directory. Images are loaded from here. Default is script starting directory.")
+    parser.add_argument('-o', '--output', nargs='?', default=sys.path[0], type=str,
+                        help="Set output directory to save cropped images.")
     parser.add_argument('-e', '--extensions', nargs='*', default=['.jpg', '.png'],
                         help="Choose file extensions to process. Must be images. Defaults are ['.jpg', '.png']")
-    parser.add_argument('-g', '--group', nargs='?', default=True, type=bool,
-                        help="Crop hover/idle images with the same sized box.")
+    parser.add_argument('-g', '--group', nargs='?', default=False, type=bool,
+                        help="Crop matched images (with '-r pattern') with the same sized box.")
+    parser.add_argument('-d', '--difference', nargs='?', default=False, type=bool,
+                        help="Get the difference of single and group crop")
+    parser.add_argument('-f', '--formatting', nargs='?', default="formatting.json", type=str,
+                        help="Path to the formatting JSON file.")
     parser.add_argument('-bw', '--border-width', nargs='*', default=0, type=int,
                         help="Set border width to add to cropping bounding box.")
     parser.add_argument('-bh', '--border-height', nargs='*', default=0, type=int,
                         help="Set border height to add to cropping bounding box.")
+    parser.add_argument('-r', '--regex-group-by', nargs='?', default="(.+)(_hover|_idle)",
+                        help="Regex to use for grouping images by for cropping, uses the first capture group and puts all identicals in the same bucket. Default is '(.+)(_hover|_idle)' to group hover and idle images.")
     parameters = vars(parser.parse_args(args))
 
     return parameters
 
 
-def get_formatting(folder:str):
-    if pathlib.Path(os.path.join(working, folder), "format.txt").exists():
-        result = pathlib.Path(os.path.join(working, folder, "format.txt")).read_text()
-    else:
-        result = 'add "{name}" xpos {x} ypos {y}\n'
-    return result
-
-
-def init_coords_file(working, cropped):
-    if pathlib.Path(os.path.join(working, "init.txt")).exists():
-        result = pathlib.Path(os.path.join(working, "init.txt")).read_text()
-        with open(pathlib.Path(os.path.join(cropped, "coords.txt")), mode='w+') as f:
-            f.write(result)
-    elif pathlib.Path(os.path.join(working, "start.txt")).exists():
-        result = pathlib.Path(os.path.join(working, "start.txt")).read_text()
-        with open(pathlib.Path(os.path.join(cropped, "coords.txt")), mode='w+') as f:
-            f.write(result)
-    else:
-        with open(pathlib.Path(os.path.join(cropped, "coords.txt")), mode='w+') as f:
-            f.write("")
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     args = parse_arguments(sys.argv[1:])
+    # args = parse_arguments(['-o', r'output/', '-d', 'True'])
+    shutil.rmtree(Path(args['output']), ignore_errors=True)
+    print(
+        f"------------\nNew run in {args['work_dir']}, with output in {args['output']}:")
 
-    working = args['work_dir']
-    # strip the dot
-    extensions = list(map(lambda x: x.replace('.', ''), args['extensions']))
-    folders_and_files, grouped_dict = get_folders_and_images(working, extensions)
+    extensions = list(map(lambda ext: ext.replace('.', ''), args['extensions']))
+
+    images = get_folders_and_images(
+        args['work_dir'], extensions, args['regex_group_by'])
+
+    disable_bar()
+
+    if args['difference']:
+        single_images = get_cropped_paths(
+                images.singles, args['work_dir'], args['output'])
+        a = zip(single_images, get_bboxes(load_singles(images.singles)))
+        grouped_images = get_grouped_output_paths(
+                images.grouped, args['work_dir'], args['output'])
+        b = zip(grouped_images, get_grouped_bboxes(images.grouped))
+        a = set({item[0]:item[1] for item in (a)})
+        b = set({item[0]:item[1] for item in (b)})
+        print("Showing differences:\n")
+        print(a - b)
+        quit()
 
     # process grouped images
     if args['group']:
-        for folder, (groups, image_count) in grouped_dict.items():
-            # create folder for cropped images
-            pathlib.Path(os.path.join(working, "cropped", folder)).mkdir(parents=True, exist_ok=True)
-            # check if custom formatting exists
-            formatting = get_formatting(folder)
-            # initiate coords file
-            init_coords_file(pathlib.Path(os.path.join(working, folder)),
-                             pathlib.Path(os.path.join(working, "cropped", folder)))
-            with alive_bar(image_count, bar='smooth', spinner='classic',
-                           title=f"Cropping images in {folder}") as files_bar:
-                for prefix, group in groups:
-                    # prefix is not needed
-                    files = [os.path.join(folder, file) for file in group]
-                    cropped_images = process_image_files(files, **args, callback=files_bar)
-                    save_file_paths = [os.path.join(working, "cropped", folder, file) for file in group]
-                    coords_file_path = os.path.join(working, "cropped", folder, "coords.txt")
-                    for cropped_image, file_name in zip(cropped_images, group):
-                        save_file_path = os.path.join(working, "cropped", folder, file_name)
-                        save_image_to_file(cropped_image.image, save_file_path)
-                        save_coordinates_to_file(pathlib.Path(os.path.join(folder, file_name)).as_posix(), cropped_image.x, cropped_image.y, coords_file_path,
-                                             formatting)
+        print("Processing files by groupings..\n")
+        grouped_images = load_grouped(images.grouped)
+        bboxes = get_grouped_bboxes(images.grouped)
+        cropped_images = get_cropped_images(grouped_images, bboxes, list(
+            itertools.repeat((args['border_width'], args['border_height']), len(bboxes))))
+        output_paths = get_grouped_output_paths(
+            images.grouped, args['work_dir'], args['output'])
+        save_images(cropped_images, output_paths)
+        substitutions = list(itertools.repeat(get_formatting(args['formatting']), len(bboxes)))
+        save_coordinates(output_paths, bboxes, substitutions)
 
-
-    # process images by themselves
+    # process single images
     else:
-        for folders in folders_and_files.items():
-            print(folders)
-            # create folder for cropped images
-            current_folder = folders[0]
-            file_names = folders[1]
-            pathlib.Path(os.path.join(working, "cropped", current_folder)).mkdir(parents=True, exist_ok=True)
-            # check if custom formatting exists
-            formatting = get_formatting(current_folder)
-            # initiate coords file
-            init_coords_file(pathlib.Path(os.path.join(working, current_folder)),
-                             pathlib.Path(os.path.join(working, "cropped", current_folder)))
-
-            with alive_bar(len(file_names), bar='smooth', spinner='classic',
-                           title=f"Cropping images in {current_folder}") as files_bar:
-                for image_file_name in file_names:
-                    # print(f"Cropping {os.path.join(working, folders[0], image_file)}")
-
-                    # process image
-                    image_file_path = os.path.join(working, current_folder, image_file_name)
-                    cropped_image = process_image_file(image_file_path, **args)
-
-                    # save image to file
-                    save_file_path = os.path.join(working, "cropped", current_folder, image_file_name)
-                    coords_file_path = os.path.join(working, "cropped", current_folder, "coords.txt")
-                    save_image_to_file(cropped_image.image, save_file_path)
-                    save_coordinates_to_file(image_file_name, cropped_image.x, cropped_image.y, coords_file_path, formatting)
-                    files_bar()
+        print("Processing files by themselves..\n")
+        single_images = load_singles(images.singles)
+        bboxes = get_bboxes(single_images)
+        cropped_images = get_cropped_images(single_images, bboxes, list(
+            itertools.repeat((args['border_width'], args['border_height']), len(bboxes))))
+        output_paths = get_cropped_paths(
+            images.singles, args['work_dir'], args['output'])
+        save_images(cropped_images, output_paths)
+        substitutions = list(itertools.repeat(get_formatting(args['formatting']), len(bboxes)))
+        save_coordinates(output_paths, bboxes, substitutions)
---
+++ processing.py
@@ -0,0 +1,228 @@
+import json
+import os
+import re
+import itertools
+from pathlib import Path
+from collections import namedtuple
+from PIL import Image
+from toolz import functoolz
+from alive_progress import alive_bar, config_handler
+
+unwrap = functoolz.compose_left(itertools.chain.from_iterable, list)
+mapl = functoolz.compose_left(map, list)
+
+
+def get_folders_and_images(path: str, extensions=["jpg", "png"], group_by: str = "(.+)(_hover|_idle)"):
+    grouped_dict = {}
+    single_dict = {}
+    all_images = namedtuple('ImageDicts', ['singles', 'grouped'])
+    # walk through all sub folders
+    for currentdir, _, files in os.walk(path):
+        # get list of files with defined extensions
+        images = list(filter(lambda filename: any(
+            f".{e}" in filename for e in extensions), files))
+        # process unless there are no image files or has "cropped" in folder path
+        if len(images) > 0 and 'cropped' not in currentdir:
+            r = re.compile(group_by)
+            grouped_images = itertools.groupby(
+                images, lambda x: r.match(x).group(1) if r.match(x) else None)
+            grouped_images = list(
+                map(lambda x: (x[0], list(x[1])), grouped_images))
+            # put them in the dicts as folder:[images] or folder:[groups]
+            single_dict[currentdir] = images
+            grouped_dict[currentdir] = grouped_images
+
+    return all_images(singles=single_dict, grouped=grouped_dict)
+
+
+def get_formatting(path: str):
+    # default formatting
+    formatting = {".*_hover": '    imagebutton:\n        hover "{path}"\n',
+                  ".*_idle": '        idle "{path}"\n        xpos {x} ypos {y} focus_mask True\n',
+                  "bg_.*|bg.*": '    add "{path}" xpos {x} ypos {y}\n',
+                  ".png": "{name} {x},{y}\n"}
+    if path:
+        p = Path(path)
+        if p.is_file() & p.suffix.__eq__(".json"):
+            formatting = json.loads(p.read_text())
+    return formatting
+
+
+def save_coordinate(path: Path, bbox, substitutions):
+    x, y = bbox[0], bbox[1]
+    bx, by = bbox[2], bbox[3]
+    name = path.name
+    if not substitutions:
+        f = "{name} {x},{y},{bx},{by}"
+    else:
+        # take first matching key
+        key = list(filter(lambda x: re.search(x, name), substitutions.keys()))
+        f = substitutions[key[0]]
+    with open(path.parent.joinpath("screen.rpy"), mode='a+') as screen:
+        screen.write(f.format(x=x, y=y, name=name, bx=bx, by=by, path=path.absolute()))
+
+
+def get_absolute_paths(kvp):
+    """
+    :param kvp: Key-Value pair of (folder,[files])
+    """
+    return [Path(os.path.join(kvp[0], file)) for file in kvp[1]]
+
+
+def load_image(path: str or Path) -> Image:
+    image = Image.open(path)
+    return image
+
+
+def save_image(image: Image, path: Path):
+    path.parent.mkdir(exist_ok=True, parents=True)
+    image.save(path)
+
+
+def get_grouped_bounding_boxes(data):
+    res = []
+    f = functoolz.compose_left(get_paths, itertools.chain.from_iterable, list, load_images, get_bboxes)
+    for group, files in data[1]:
+        bboxes = f([(data[0], files)])
+        tx = min(bboxes, key=lambda x: x[0])[0]
+        ty = min(bboxes, key=lambda x: x[1])[1]
+        bx = max(bboxes, key=lambda x: x[2])[2]
+        by = max(bboxes, key=lambda x: x[3])[3]
+        biggest = (tx, ty, bx, by)
+        res.append(list(itertools.repeat(biggest, len(files))))
+    res = unwrap(res)
+    return res
+
+
+def get_grouped_paths_helper(data):
+    res = []
+    for _, files in data[1]:
+        res.append([Path(data[0]).joinpath(x) for x in files])
+    res = unwrap(res)
+    return res
+
+
+def get_grouped_output_paths_helper(data, output, relpath):
+    res = []
+    for _, files in data[1]:
+        res.append([Path(output).joinpath(relpath).joinpath(x) for x in files])
+    res = unwrap(res)
+    return res
+
+
+def get_grouped_output_paths(images_dict: dict, input_dir: str, output: str = None):
+    if not input_dir:
+        input_dir = os.path.commonpath(images_dict.keys())
+    rel_paths = [os.path.relpath(p, input_dir) for p in images_dict.keys()]
+    if not output:
+        output = input_dir
+    return unwrap(
+        [get_grouped_output_paths_helper(item, output, rel) for item, rel in zip(images_dict.items(), rel_paths)])
+
+
+def get_bounding_box(image: Image):
+    return image.getbbox()
+
+
+def get_cropped_paths(images_dict: dict, input_dir: str, output: str = None):
+    if not input_dir:
+        input_dir = os.path.commonpath(images_dict.keys())
+    rel_paths = [os.path.relpath(p, input_dir) for p in images_dict.keys()]
+    if not output:
+        output = input_dir
+    cropped_paths = get_paths([(os.path.join(output, p[1]), p[0][1])
+                               for p in zip(images_dict.items(), rel_paths)])
+    return list(itertools.chain.from_iterable(cropped_paths))
+
+
+def get_cropped_image(image, bbox, border=(0, 0)):
+    # Crop the image to the contents of the bounding box
+    cropped = image.crop(bbox)
+    # Determine the width and height of the cropped image
+    (width, height) = cropped.size
+    # Add border
+    width += border[0] * 2
+    height += border[1] * 2
+    # Create a new image object for the output image
+    cropped_image = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+    # Paste the cropped image onto the new image
+    cropped_image.paste(cropped, (border[0], border[1]))
+    return cropped_image
+
+
+def func_with_progressbar(callback, func, *data):
+    res = func(*data)
+    callback.text = data
+    callback()
+    return res
+
+
+def get_len(data):
+    if hasattr(data, '__len__'):
+        return len(data)
+    else:
+        return len(list(data))
+
+
+def mapl_with_progress(func, *data, **kwargs):
+    size = get_len(min(data, key=get_len))
+    with alive_bar(size, **kwargs) as bar:
+        f = functoolz.partial(func_with_progressbar, bar)
+        g = functoolz.partial(f, func)
+        return mapl(g, *data)
+
+
+def disable_bar():
+    config_handler(disable=True)
+
+
+""" [paths:str] -> [(int,int,int,int)] """
+get_bboxes = functoolz.partial(
+    mapl_with_progress, get_bounding_box, title="Getting boundings".ljust(20), bar="filling", dual_line=True)
+
+""" [(folder:str,[files:str])] -> [paths:str] """
+get_paths = functoolz.partial(mapl, get_absolute_paths)
+
+""" [path:str] -> [image:Image] """
+load_images = functoolz.partial(
+    mapl_with_progress, load_image, title="Loading images".ljust(20), bar="filling", dual_line=True)
+
+""" [image:Image], [bbox:(int,int,int.int)], [border:(int,int)] -> [image:Image] """
+get_cropped_images = functoolz.partial(
+    mapl_with_progress, get_cropped_image, title="Cropping images".ljust(20), bar="filling", dual_line=True)
+
+""" [image:Image, path:str] -> None """
+save_coordinates = functoolz.partial(
+    mapl_with_progress, save_coordinate, title="Saving coordinates".ljust(20), bar="filling", dual_line=True)
+
+""" [image:Image, path:str] -> None """
+save_images = functoolz.partial(
+    mapl_with_progress, save_image, title="Saving images".ljust(20), bar="filling", dual_line=True)
+
+""" {(folder:str,[files:str])} -> [image:image] """
+load_singles = functoolz.compose_left(
+    lambda x: x.items(),
+    # make them all to path strings for paths of [[folder1][folder2]...[folder-n]]
+    get_paths,
+    itertools.chain.from_iterable,  # flatten list of lists to [paths]
+    list,
+    load_images,
+)
+
+get_grouped_bboxes = functoolz.compose_left(lambda x: x.items(),
+                                            functoolz.partial(mapl_with_progress, get_grouped_bounding_boxes,
+                                                              title="Getting grouped bboxes".ljust(20), bar="filling",
+                                                              dual_line=True),
+                                            unwrap)
+
+get_grouped_paths = functoolz.partial(mapl_with_progress, get_grouped_paths_helper,
+                                      title="Getting grouped paths".ljust(20), bar="filling", dual_line=True)
+
+load_grouped = functoolz.compose_left(
+    lambda x: x.items(),
+    # make them all to path strings for paths of [[folder1][folder2]...[folder-n]]
+    get_grouped_paths,
+    itertools.chain.from_iterable,  # flatten list of lists to [paths]
+    list,
+    load_images,
+)
